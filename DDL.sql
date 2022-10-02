65d2d907b79865e91e0bfdb65a8d740b6fb4968d
Zabil Sabri Muhammad <79674049+zabilsabri@users.noreply.github.com>
2022-10-03 01:45:10 +0800

    Add files via upload

---
+++ DDL.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE contract_sisfo;
+
+USE contract_sisfo;
+
+CREATE TABLE student_info(
+	id INT AUTO_INCREMENT,
+	full_name VARCHAR (225),
+	address VARCHAR (225),
+	major VARCHAR (100),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE course(
+	course_id INT AUTO_INCREMENT,
+	course_name VARCHAR (200),
+	duration INT,
+	`date` DATE,
+	`time` TIME,
+	PRIMARY KEY (course_id)
+);
+
+CREATE TABLE `schedule` (
+	`schedule_id` INT AUTO_INCREMENT,
+	id INT,
+	course_id INT,
+	`time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (id) REFERENCES student_info(id),
+	FOREIGN KEY (course_id) REFERENCES course(course_id),
+	PRIMARY KEY (`schedule_id`)
+);
---
+++ DML.sql
@@ -0,0 +1,25 @@
+-- 1
+INSERT INTO student_info (full_name, address, Major) VALUES ('Emilia Damayanto', 'Jln. Sahabat', 'Teknik');
+
+-- 2
+SELECT * FROM student_info;
+
+-- 3
+UPDATE student_info SET full_name = 'Emilia Damayanti', Address = 'Jln. Kumala', Major= 'Sisfo' WHERE id = 4;
+
+-- 4
+SELECT student_info.id, course.course_name, course.`date`, course.`time` from student_info 
+inner JOIN `schedule` 
+ON `schedule`.id = student_info.id 
+inner join course 
+on course.course_id = `schedule`.course_id
+WHERE `schedule`.id = 3;
+
+-- 5
+SELECT course_id, course_name, duration FROM course;
+
+-- 6
+INSERT INTO `schedule` (id, course_id) VALUES (4, 3);
+
+-- 7
+UPDATE `schedule` SET id = 4, course_id = 2 WHERE id = 4;
