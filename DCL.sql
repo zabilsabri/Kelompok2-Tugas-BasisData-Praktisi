ddcdd8afa3d53f26ec6a46fcdb100e43f8c80fb2
aimlana <salim.yaklu@gmail.com>
2022-10-10 20:32:40 +0800

    final

--- DCL.sql
+++ DCL.sql
@@ -1,10 +1,21 @@
 # Buat user baru
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
 CREATE USER 'DosenSisfo'@'localhost' IDENTIFIED BY 'dosensisfo.id';
+CREATE USER 'mahasiswa'@'localhost' IDENTIFIED BY 'mahasiswa';
 
 # Beri hak akses ke user 
+GRANT ALL   
+ON contract_sisfo
+TO 'admin'@'localhost';
+
 GRANT CREATE, DROP, SELECT, INSERT, DELETE, UPDATE, ALTER  
 ON contract_sisfo
 TO 'DosenSisfo'@'localhost';
+
+GRANT CREATE, DROP, SELECT, INSERT, DELETE, UPDATE, ALTER  
+ON contract_sisfo
+TO 'mahasiswa'@'localhost';
+
 
 # Cek status hak akses user
 SHOW GRANTS FOR 'DosenSisfo'@'localhost';
@@ -13,3 +24,8 @@ SHOW GRANTS FOR 'DosenSisfo'@'localhost';
 REVOKE CREATE, DROP, ALTER 
 ON contract_sisfo
 FROM 'DosenSisfo'@'localhost';
+
+REVOKE CREATE, DROP, ALTER 
+ON contract_sisfo
+FROM 'mahasiswa'@'localhost';
+
